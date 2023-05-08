7ff01986f54078cc65186b73d452be68bcd2b026
lmx <2538381255@qq.com>
2023-05-08 22:33:10 +0800

    修复

--- src/components/LayoutContainer/index.vue
+++ src/components/LayoutContainer/index.vue
@@ -76,75 +76,76 @@ const enabledRightSide = computed(() => {
 
 <style lang="scss" scoped>
 .flex-container {
-    position: absolute;
-    width: 100%;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  padding: 20px;
+
+  .left-side,
+  .right-side,
+  .main {
+    --container-padding: 15px;
+
     height: 100%;
-    display: flex;
-    padding: 20px;
+    padding: var(--container-padding);
+    background-color: var(--g-app-bg);
+    transition: background-color 0.3s;
+  }
 
-    .left-side,
-    .right-side,
-    .main {
-        --container-padding: 15px;
+  .left-side {
+    margin-right: 20px;
+    flex: none;
+    overflow: auto;
+  }
 
-        height: 100%;
-        padding: var(--container-padding);
-        background-color: var(--g-app-bg);
-        transition: background-color 0.3s;
+  .right-side {
+    margin-left: 20px;
+    flex: none;
+    overflow: auto;
+  }
+
+  .main {
+    flex: 1;
+    position: relative;
+    width: 100%;
+
+    .left-side-icon,
+    .right-side-icon {
+      z-index: 1;
+      font-size: 18px;
+      background-color: var(--g-app-bg);
+      color: var(--el-text-color-placeholder);
+      transition: background-color 0.3s, var(--el-transition-color);
+      border-radius: 50%;
+      width: 24px;
+      height: 24px;
+      cursor: pointer;
+
+      @include position-center(y);
+
+      &:hover {
+        color: var(--el-text-color-regular);
+      }
     }
 
-    .left-side {
-        margin-right: 20px;
-        flex: none;
-        overflow: auto;
+    .left-side-icon {
+      left: -10px;
     }
 
-    .right-side {
-        margin-left: 20px;
-        flex: none;
-        overflow: auto;
+    .right-side-icon {
+      right: -10px;
     }
 
-    .main {
-        flex: 1;
-        position: relative;
-        width: 100%;
-
-        .left-side-icon,
-        .right-side-icon {
-            font-size: 18px;
-            background-color: var(--g-app-bg);
-            color: var(--el-text-color-placeholder);
-            transition: background-color 0.3s, var(--el-transition-color);
-            border-radius: 50%;
-            width: 24px;
-            height: 24px;
-            cursor: pointer;
-
-            @include position-center(y);
-
-            &:hover {
-                color: var(--el-text-color-regular);
-            }
-        }
-
-        .left-side-icon {
-            left: -10px;
-        }
-
-        .right-side-icon {
-            right: -10px;
-        }
-
-        .main-container {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            padding: var(--container-padding);
-            overflow: auto;
-        }
+    .main-container {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      padding: var(--container-padding);
+      overflow: auto;
     }
+  }
 }
 </style>
