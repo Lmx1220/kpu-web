0c7c20e99254c6a99e9c357b121a6f9ecf94f6ed
lmx <2538381255@qq.com>
2023-10-24 01:29:44 +0800

    添加字典类型

---
+++ src/enums/common/yesOrNoEnum.ts
@@ -0,0 +1,20 @@
+export enum YesOrNoEnum {
+  Yes = 1,
+  No = 0,
+}
+export default {
+  dic: [
+    {
+      label: '是',
+      value: true,
+    },
+    {
+      label: '否',
+      value: false,
+    },
+  ],
+  false: '否',
+  true: '是',
+  yes: false,
+  no: true,
+}
