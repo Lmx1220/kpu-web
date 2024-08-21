3445d4b76f896a002535111f1c4b6a716052c532
lmx <2538381255@qq.com>
2024-08-21 12:48:30 +0800

    修复ui切换的底层兼容问题

--- src/components/LoginSwitcher/index.vue
+++ src/components/LoginSwitcher/index.vue
@@ -4,28 +4,27 @@ defineOptions({
 })
 const route = useRoute()
 const router = useRouter()
+
 function command(name: string) {
   router.replace({ name })
 }
+const items = computed(() => {
+  return [[
+    { label: '风格1', handle: () => command('login'), disabled: route.name === 'login' },
+    { label: '风格2', handle: () => command('login2'), disabled: route.name === 'login2' },
+  ]]
+})
 </script>
 
 <template>
   <div>
-    <ElDropdown @command="command">
-      <ElButton>
+    <HDropdownMenu
+      :items="items"
+    >
+      <HButton outline>
         切换登录页风格
         <SvgIcon class="el-icon--right" name="i-ep:arrow-down" />
-      </ElButton>
-      <template #dropdown>
-        <ElDropdownMenu>
-          <ElDropdownItem :disabled="route.name === 'login'" command="login">
-            风格1
-          </ElDropdownItem>
-          <ElDropdownItem :disabled="route.name === 'login2'" command="login2">
-            风格2
-          </ElDropdownItem>
-        </ElDropdownMenu>
-      </template>
-    </ElDropdown>
+      </HButton>
+    </HDropdownMenu>
   </div>
 </template>
