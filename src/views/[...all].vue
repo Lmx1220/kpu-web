8eb75f494b434155198e72b8c0cde7a2912de4b2
lmx <2538381255@qq.com>
2024-08-21 12:49:04 +0800

    修复ui切换的底层兼容问题

--- src/views/[...all].vue
+++ src/views/[...all].vue
@@ -1,26 +1,25 @@
-<script lang="ts" setup>
-import useSettingsStore from '@/store/modules/settings.ts'
+
+<script setup lang="ts">
+import useSettingsStore from '@/store/modules/settings'
 
 const router = useRouter()
+
 const settingsStore = useSettingsStore()
 
-const data = ref<{
-  inter: IntervalHandle | number
-  countdown: number
-}>({
+const data = ref({
   inter: Number.NaN,
   countdown: 5,
 })
 
 onBeforeRouteLeave(() => {
-  data.value.inter && clearInterval(data.value.inter)
+  data.value.inter && window.clearInterval(data.value.inter)
 })
 
 onMounted(() => {
-  data.value.inter = setInterval(() => {
+  data.value.inter = window.setInterval(() => {
     data.value.countdown--
     if (data.value.countdown === 0) {
-      data.value.inter && clearInterval(data.value.inter)
+      data.value.inter && window.clearInterval(data.value.inter)
       goBack()
     }
   }, 1000)
@@ -32,46 +31,20 @@ function goBack() {
 </script>
 
 <template>
-  <div class="notfound">
-    <SvgIcon name="404" class="icon" />
-    <div class="content">
-      <h1>404</h1>
-      <div class="desc">
+  <div class="absolute left-[50%] top-[50%] flex flex-col items-center justify-between lg:flex-row -translate-x-50% -translate-y-50% lg:gap-12">
+    <SvgIcon name="404" class="text-[300px] lg:text-[400px]" />
+    <div class="flex flex-col gap-4">
+      <h1 class="m-0 text-6xl font-sans">
+        404
+      </h1>
+      <div class="desc mx-0 text-xl text-stone-5">
         抱歉，你访问的页面不存在
       </div>
-      <ElButton type="primary" @click="goBack">
-        {{ data.countdown }} 秒后，返回首页
-      </ElButton>
+      <div>
+        <HButton @click="goBack">
+          {{ data.countdown }} 秒后，返回首页
+        </HButton>
+      </div>
     </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.notfound {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 700px;
-
-  @include position-center(xy);
-
-  .icon {
-    width: 400px;
-    height: 400px;
-  }
-
-  .content {
-    h1 {
-      margin: 0;
-      font-size: 72px;
-      color: var(--el-text-color-primary);
-    }
-
-    .desc {
-      margin: 20px 0 30px;
-      font-size: 20px;
-      color: var(--el-text-color-secondary);
-    }
-  }
-}
-</style>
