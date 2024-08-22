7ef244c82c761f8fd4c3181de1defa108b8bb168
lmx <2538381255@qq.com>
2024-08-22 10:53:13 +0800

    测试

--- src/api/modules/user.ts
+++ src/api/modules/user.ts
@@ -1,4 +1,5 @@
 import defHttp from '@/api'
+import { baseRequestClient } from '@/api/requestClient.ts'
 
 export function login(data: {
   account: string
@@ -16,6 +17,17 @@ export function favorites() {
   return defHttp.get({
     url: '/user/favorites',
     baseURL: '/mock/',
+  })
+}
+/**
+ * 刷新accessToken
+ */
+export async function refreshTokenApi() {
+  return baseRequestClient.post<{
+    data: string
+    status: number
+  }>('/mock/auth/refresh', {
+    withCredentials: true,
   })
 }
 
