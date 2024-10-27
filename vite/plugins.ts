5f443a9362fe0deb0556e01e92ac0d23b5fea261
lmx <2538381255@qq.com>
2024-10-28 02:27:14 +0800

    test: pwa测试

--- vite/plugins.ts
+++ vite/plugins.ts
@@ -154,27 +154,27 @@ export default function createVitePlugins(mode: string, isBuild = false) {
       devOptions: {
         enabled: true,
       },
-      // base: './',
-      // manifest: {
-      //   name: 'admin 专业版',
-      //   short_name: 'admin 专业版',
-      //   description: '一款开箱即用的 Vue 中后台管理系统框架',
-      //   display: 'standalone',
-      //   background_color: '#fff',
-      //   theme_color: '#e60000',
-      //   icons: [
-      //     {
-      //       src: './pwa_icons/192x192.png',
-      //       sizes: '192x192',
-      //       type: 'image/png',
-      //     },
-      //     {
-      //       src: './pwa_icons/384x384.png',
-      //       sizes: '384x384',
-      //       type: 'image/png',
-      //     },
-      //   ],
-      // },
+      base: './',
+      manifest: {
+        name: 'admin 专业版',
+        short_name: 'admin 专业版',
+        description: '一款开箱即用的 Vue 中后台管理系统框架',
+        display: 'standalone',
+        background_color: '#fff',
+        theme_color: '#e60000',
+        icons: [
+          {
+            src: './pwa_icons/192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './pwa_icons/384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+        ],
+      },
     }),
 
     {
